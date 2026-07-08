43a3b344c65bbf635f20a813035a638d908b558a
rohit00112 <subedirohit49@gmail.com>
2026-07-08 12:52:41 +0545

    chore: add microsoft clarity

--- components/common/MarketingScripts.tsx
+++ components/common/MarketingScripts.tsx
@@ -7,6 +7,7 @@ const normalizeTrackingId = (value?: string) => {
 
 const GTM_ID = normalizeTrackingId(process.env.NEXT_PUBLIC_GTM_ID);
 const META_PIXEL_ID = normalizeTrackingId(process.env.NEXT_PUBLIC_META_PIXEL_ID);
+const CLARITY_ID = normalizeTrackingId(process.env.NEXT_PUBLIC_CLARITY_ID) || 'xj4daxfxrz';
 
 const MarketingScripts = () => (
   <>
@@ -35,6 +36,18 @@ const MarketingScripts = () => (
           'https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '${META_PIXEL_ID}');
           fbq('track', 'PageView');
+        `}
+      </Script>
+    )}
+
+    {CLARITY_ID && (
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "${CLARITY_ID}");
         `}
       </Script>
     )}
