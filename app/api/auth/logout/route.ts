fba1a2893377d2876ac616c2ef639bfa30145c0d
rohit00112 <subedirohit49@gmail.com>
2026-07-14 15:44:37 +0545

    fix: fix logout url

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -3,5 +3,5 @@ import { logout } from '../../../../lib/auth';
 
 export async function POST(request: Request) {
   await logout();
-  return NextResponse.redirect(new URL('/login', request.url), 303);
+  return NextResponse.redirect('/login', 303);
 }
